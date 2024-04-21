98977273aae861a2a7bbf5abdda98dc7c4f32063
malga <miamalgarini.mm@gmail.com>
2024-04-21 12:05:46 -0300

    Inicializacion proyecto e-commerce

---
+++ src/componentes/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <a href="#">Productos</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Contacto</a>
+        </nav>
+    );
+}
+
+export default Navbar;
