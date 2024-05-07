551787d4751cb0f64439a7415c313e7804210f0a
Mauro Laime <maurol.dev@gmail.com>
2024-05-07 13:59:35 -0300

    feat: set up folder structure and implement ESLint rules

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './navbar.css';
-
+import './navbar.css'
 
 
 function Navbar() {
+
+    console.log("hola ");
+
     return (
         <nav className="navbar">
             <a href="#">Productos</a>
